480deaf6c410cabe82d84b12bc502fdba5fa99fb
imukovin <mukovin2013@yandex.ru>
2019-09-15 11:19:05 +0600

    FileVisitor realised

--- module9/1/FolderSize/src/Loader.java
+++ module9/1/FolderSize/src/Loader.java
@@ -1,56 +1,68 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
 import java.util.Scanner;
+
+import static java.lang.Integer.MAX_VALUE;
 
 public class Loader {
     private static final Scanner in = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        for (;;) {
-            File folder = getPath();
-            File[] listFiles = folder.listFiles();
+    private static long dirSize;
 
-            long dirSize = 0;
-            for (File l : listFiles) {
-                dirSize += getDirSize(new File(l.toString()));
-            }
+    public static void main(String[] args) throws IOException {
+        for (;;) {
+            Path path = getPath();
+            dirSize = 0;
+
+            Files.walkFileTree(path, new HashSet<>(), MAX_VALUE, new FileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                        throws IOException {
+                    //System.out.println("visitFile: " + file);
+                    dirSize += file.toFile().length();
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path path, IOException e) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path path, IOException e) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
 
             convertSizePrint(dirSize);
         }
     }
 
-    public static File getPath() {
+    public static Path getPath() {
+        Path p;
         File f;
-
         for (;;) {
             System.out.println("Введите путь к папке: ");
             String path = in.next();
 
             f = new File(path);
             if (f.isDirectory()) {
+                p = Paths.get(path);
                 break;
             } else {
                 System.out.println(path + " - не путь к папке!");
             }
         }
-
-        return f;
-    }
-
-    public static long getDirSize(File dir) {
-        long size = 0;
-        if (dir.isFile()) {
-            size = dir.length();
-        } else {
-            File[] subFiles = dir.listFiles();
-            for (File file : subFiles) {
-                if (file.isFile()) {
-                    size += file.length();
-                } else {
-                    size += getDirSize(file);
-                }
-            }
-        }
-        return size;
+        return p;
     }
 
     public static void convertSizePrint(double size) {
