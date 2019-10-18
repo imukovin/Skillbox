4072c754e2a74108af2c6280c8d3835cc86dfa58
imukovin <mukovin2013@yandex.ru>
2019-10-18 17:13:40 +0600

    Edits

--- module11/1/ImageResizer/src/ImageResizer.java
+++ module11/1/ImageResizer/src/ImageResizer.java
@@ -2,14 +2,13 @@ import javax.imageio.ImageIO;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
-import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
 
 public class ImageResizer extends Thread {
     private static final String OUT_FOLDER = "outFolder/";
-    private static final int SIZE_FIRST_CHANGE_W = 600;
-    private static final int SIZE_SECOND_CHANGE_W = 300;
+    private static final int OUTPUT_WIDTH = 300;
+    private static final int MEDIUM_WIDTH = OUTPUT_WIDTH * 2;
 
     private File[] files;
 
@@ -29,53 +28,35 @@ public class ImageResizer extends Thread {
                     continue;
                 }
 
-                int size = 0;
-                if (image.getWidth() > image.getHeight()) {
-                    size = image.getWidth();
-                } else {
-                    size = image.getHeight();
-                }
+                BufferedImage firstStepScale = getNewImage(image, 1, MEDIUM_WIDTH);
+                BufferedImage outputImage = getNewImage(firstStepScale, 2, OUTPUT_WIDTH);
 
-                int firstChangeHeight = (int) Math.round(image.getHeight() / (image.getWidth() / (double) SIZE_FIRST_CHANGE_W));
-
-                double trans=1.0/(size/SIZE_FIRST_CHANGE_W);
-
-                AffineTransform tr = new AffineTransform ();
-                tr.scale(trans, trans);
-                AffineTransformOp op = new AffineTransformOp(tr, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-                Double w = (double) SIZE_FIRST_CHANGE_W;
-                Double h = (double) firstChangeHeight;
-                BufferedImage image2 = new BufferedImage(w.intValue(), h.intValue(), image.getType());
-                op.filter(image, image2);
-
-                if (image2.getWidth() > image2.getHeight()) {
-                    size = image2.getWidth();
-                } else {
-                    size = image2.getHeight();
-                }
-
-                double trans1=1.0/(size/SIZE_SECOND_CHANGE_W);
-                int secondChangeHeight = (int) Math.round(image2.getHeight() / (image2.getWidth() / (double) SIZE_SECOND_CHANGE_W));
-
-                AffineTransform tr1 = new AffineTransform ();
-                tr1.scale(trans1, trans1);
-                AffineTransformOp op1 = new AffineTransformOp(tr1, AffineTransformOp.TYPE_BICUBIC);
-                Double w1 = (double) SIZE_SECOND_CHANGE_W;
-                Double h1 = (double) secondChangeHeight;
-                BufferedImage image3 = new BufferedImage(w1.intValue(), h1.intValue(), image2.getType());
-                op1.filter(image2, image3);
-
-                try {
-                    File newFile = new File(OUT_FOLDER + file.getName());
-                    ImageIO.write(image3, "jpg", newFile);
-                } catch (IOException e) {
-
-                }
+                File newFile = new File(OUT_FOLDER + file.getName());
+                ImageIO.write(outputImage, "jpg", newFile);
             }
         }
         catch (Exception ex) {
             ex.printStackTrace();
         }
         System.out.println("Время: " + (System.currentTimeMillis() - start));
+    }
+
+    private BufferedImage getNewImage(BufferedImage image, int numOfMethod, int width) {
+        int size = Math.max(image.getWidth(), image.getHeight());
+        int height = (int) Math.round(image.getHeight() / (image.getWidth() / (double) width));
+        double trans = 1.0 / (size / width);
+
+        AffineTransform tr = AffineTransform.getScaleInstance(trans, trans);
+        AffineTransformOp op;
+        if (numOfMethod == 1) {
+            op = new AffineTransformOp(tr, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+        } else {
+            op = new AffineTransformOp(tr, AffineTransformOp.TYPE_BILINEAR);
+        }
+
+        BufferedImage newImage = new BufferedImage(width, height, image.getType());
+        op.filter(image, newImage);
+
+        return newImage;
     }
 }
--- module11/1/ImageResizer/src/Main.java
+++ module11/1/ImageResizer/src/Main.java
@@ -1,24 +1,37 @@
 import java.io.File;
 import java.util.Arrays;
+import java.util.concurrent.*;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
         String srcFolder = "res/";
         File srcDir = new File(srcFolder);
         File[] files = srcDir.listFiles();
 
+        long start = System.currentTimeMillis();
         int cores = Runtime.getRuntime().availableProcessors();
 
-        if (files.length < cores) {
-            new ImageResizer(files).start();
-        } else {
-            int countForOne = files.length / cores;
-            for (int i = 0; i < cores; i++) {
-                File[] f = Arrays.copyOfRange(files, i * countForOne, (i + 1) * countForOne);
-                new ImageResizer(f).start();
+        if (files != null) {
+            if (files.length < cores) {
+                new ImageResizer(files);
+            } else {
+                int countForOne = files.length / cores;
+                ExecutorService service = Executors.newFixedThreadPool(cores);
+                for (int i = 0; i < cores; i++) {
+                    File[] f = Arrays.copyOfRange(files, i * countForOne, (i + 1) * countForOne);
+                    //service.submit(new ImageResizer(f));
+                    Thread t = new ImageResizer(f);
+                    //t.start();
+                    //t.join();
+
+                    service.submit(t);
+                }
+                service.shutdown();
             }
+        } else {
+            System.out.println("Problem with folder!");
         }
+
+        System.out.println("Общее время: " + (System.currentTimeMillis() - start));
     }
 }
