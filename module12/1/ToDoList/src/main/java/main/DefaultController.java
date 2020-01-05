795bcae06dc8b49e2fb69533334b87bf8bc5224e
imukovin <mukovin2013@yandex.ru>
2020-01-05 16:59:17 +0600

    module12_l1

---
+++ module12/1/ToDoList/src/main/java/main/DefaultController.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class DefaultController {
+    @RequestMapping("/")
+    public String getDate() {
+        return (new Date()).toString();
+    }
+}
