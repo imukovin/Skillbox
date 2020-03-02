ead0961a9172910e95cf64cc365f86f6b4fe799e
imukovin <mukovin2013@yandex.ru>
2020-03-02 21:52:27 +0600

    Threads

--- module14/1/CarNumberGenerator1/src/Loader.java
+++ module14/1/CarNumberGenerator1/src/Loader.java
@@ -1,20 +1,24 @@
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Loader
 {
-    public static final int NUM_OF_THREADS = 100;
+    private static final int NUM_OF_THREADS = 100;
 
     public static void main(String[] args) throws InterruptedException {
         long start = System.currentTimeMillis();
+        ExecutorService taskExecutor = Executors.newFixedThreadPool(NUM_OF_THREADS);
 
         for (int i = 1; i <= NUM_OF_THREADS; i++) {
             String fileName = "res/numbers" + i + ".txt";
             int regionCode = i;
-            Thread t = new Thread(new Runrun(fileName, regionCode));
-            t.start();
-            t.join();
+            taskExecutor.execute(new Thread(new Runrun(fileName, regionCode)));
         }
-
+        taskExecutor.shutdown();
+        taskExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
         System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 }
